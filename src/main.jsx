5e505ba6a85c1797684f070254817877b7b95680
user.name like "Asif Ekhlaque <asifekhlaque33@gmail.com>
2025-11-09 18:34:49 +0530

    dynamic routing

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,9 @@ import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
+
         <App />
+
         <ToastContainer
             position="top-right"
             autoClose={5000}
@@ -19,5 +21,6 @@ createRoot(document.getElementById('root')).render(
             pauseOnHover
             theme="dark"
         />
+
     </BrowserRouter>
 )
